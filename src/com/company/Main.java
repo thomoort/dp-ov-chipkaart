ef757495c83e82522945dfcd75e1447f9e9cb29f
thomvoort <thom.vanoort@student.hu.nl>
2021-09-12 20:40:15 +0200

    commit main DP1

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.sql.*;
+import java.util.Properties;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException {
+
+        String url = "jdbc:postgresql://localhost/ov-chipkaart";
+        Properties props = new Properties();
+        props.setProperty("user","postgres");
+        props.setProperty("password","admin");
+        Connection conn = DriverManager.getConnection(url, props);
+
+        PreparedStatement statement = conn.prepareStatement("SELECT * FROM reiziger");
+        ResultSet resultSet = statement.executeQuery();
+
+        System.out.println("Alle reizigers:");
+        while (resultSet.next()) {
+            String id = resultSet.getString("reiziger_id");
+            String voorletters = resultSet.getString("voorletters").replaceAll("", ".").substring(1);
+            String tussenvoegsel = resultSet.getString("tussenvoegsel") == null ?
+                    "" : resultSet.getString("tussenvoegsel");
+            String achternaam = resultSet.getString("achternaam");
+            String geboortedatum = resultSet.getString("geboortedatum" )== null ?
+                    "" : String.format("(%s)",resultSet.getString("geboortedatum"));
+
+            String output = String.format("#%s: %s %s %s %s\n", id, voorletters, tussenvoegsel, achternaam, geboortedatum).replaceAll("\\s+", " ");
+            System.out.println("\t" + output);
+        }
+
+    }
+
+}
